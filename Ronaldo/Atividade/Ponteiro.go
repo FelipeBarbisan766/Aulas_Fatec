30263346f370493ebba5596776fd14152b00e548
FelipeBarbisan766 <felipebarbisan766@gmail.com>
2025-04-22 19:03:56 -0300

    update

--- Ronaldo/Atividade/Ponteiro.go
+++ Ronaldo/Atividade/Ponteiro.go
@@ -1,15 +1,12 @@
 package main
 
-import (
-	"fmt"
-)
-
 func main() {
 	//! 1
 	// a := 10
 	// v := &a
 	// fmt.Println("Conteúdo da variável: ", *v)
 	// fmt.Println("Endereço da variável: ", &v)
+
 	//! 2
 	// 	f := 10.5
 	// 	alteraPorValor(f)
@@ -24,7 +21,7 @@ func main() {
 	// 	ptest := &testaFloat
 	// 	alteraPorReferencia(ptest)
 	// 	fmt.Println("Valor fora da função:", testaFloat)
-	// 	fmt.Println("----------------------------")	
+	// 	fmt.Println("----------------------------")
 	// }
 	// func alteraPorValor(f float64) {
 	// 	f = 522.65
@@ -33,9 +30,23 @@ func main() {
 	// func alteraPorReferencia(p *float64) {
 	//#	*p = 331.33
 	// 	fmt.Println("Valor dentro da função:", *p)
-	//! 4
+
+	//! EX 3
+	// Vantagens:
+	// 1. Acesso rápido a memória:
+	// Ponteiros permitem acesso direto aos endereços de memória, o que pode resultar em maior velocidade de execução em comparação com acessos indiretos via índices de array.
+	// 2. Manipulação de dados complexos:
+	// Ponteiros são fundamentais para implementação de estruturas de dados como listas ligadas, árvores e outras estruturas que exigem manipulação dinâmica da memória.
+	// Desvantagens:
+	// 1. Risco de erros de memória:
+	// Erros ao manipular ponteiros, como apontar para memória inválida, podem causar travamentos, erros de execução ou vazamentos de memória.
+	// 2. Complexidade do código:
+	// A manipulação de ponteiros pode tornar o código mais difícil de entender e manter, especialmente para programadores menos experientes.
+
+	//! EX 4
 	// Falso, o uso de ponteiros pode até melhorar o desempenho em algumas partes, mas ele por si só não,
 	// a melhora do desempenho depende de outros fatores além desse.
+
 	//!EX 05
 	//  a, b, c, d, e := 10, 20, 30, 40, 50
 	//  arr := [5]*int{&a, &b, &c, &d, &e}
